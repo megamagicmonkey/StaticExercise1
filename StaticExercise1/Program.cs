4b9a18bf3b6ee9210cd3b72dcb5971671dbe62ab
megamagicmonkey <megamagicmonkey@gmail.com>
2021-09-01 13:49:32 -0500

    intitial commit

---
+++ StaticExercise1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace StaticExercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(TempConverter.CtoF(0));
+            Console.WriteLine(TempConverter.CtoF(32.1));
+            Console.WriteLine(TempConverter.FtoC(32));
+            Console.WriteLine(TempConverter.FtoC(89.78));
+        }
+    }
+}
---
+++ StaticExercise1/TempConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise1
+{
+    static class TempConverter
+    {
+
+
+        public static double FtoC(double fahrenheit)
+        {
+            double celsius = (fahrenheit - 32) * .5556;
+            
+
+            return celsius;
+        }
+
+        public static double CtoF(double celsius)
+        {
+           
+
+            double fahrenheit = (celsius * 1.8) + 32;
+
+            return fahrenheit;
+        }
+    }
+}
